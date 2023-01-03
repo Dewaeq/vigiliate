707e5d3629f5f75d1dee8b8530b590990ba7c085
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2023-01-03 20:50:46 +0100

    remove unused code

--- src/main.h
+++ src/main.h
@@ -1,18 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <Arduino.h>
-
-#define DISPL_WIDTH 128
-#define DISPL_HEIGHT 64
-#define STATUS_HEIGHT 15
-
-#define DISPL_MOSI 13 // D7
-#define DISPL_CLK 14  // D5
-#define DISPL_RES 2	  // D4
-#define DISPL_DC 0	  // D3
-#define DISPL_CS 15	  // D8
-
 void setupWebServer();
 
 // routes
