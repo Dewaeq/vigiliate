8f0fb994bce831e7cb30c612ec9bdd6320622f64
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2023-01-07 21:35:54 +0100

    show sensor readings on display

--- src/sensors/sensors.cpp
+++ src/sensors/sensors.cpp
@@ -1,4 +1,5 @@
 #include "sensors.h"
+#include "gui.h"
 
 void SensorsClass::begin() {
     sds.begin();
@@ -8,12 +9,23 @@ void SensorsClass::begin() {
 void SensorsClass::update() {
     sds.update();
     dht.update();
+    getReadings();
 }
 
 SensorsReading SensorsClass::getReadings() {
     SensorsReading reading;
     reading.sds = sds.result;
     reading.dht = dht.result;
+
+    Gui.setBody("Temp: ");
+    Gui.appendBody(String(reading.dht.temperature));
+    Gui.appendBody("Hum: ", true);
+    Gui.appendBody(String(reading.dht.humitidy));
+    Gui.appendBody("PM2.5: ", true);
+    Gui.appendBody(String(reading.sds.pm25));
+    Gui.appendBody("PM10: ", true);
+    Gui.appendBody(String(reading.sds.pm10));
+    Gui.show();
 
     return reading;
 }
