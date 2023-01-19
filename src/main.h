697516712cdec29defcbf10a026c2c339b83d536
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2023-01-19 21:01:27 +0100

    remove unused headers

--- src/main.h
+++ src/main.h
@@ -2,14 +2,6 @@
 #define MAIN_H
 
 void setupWebServer();
-
-// routes
-void handleRoot();
-void handleDisconnect();
-void handleReadings();
-void handleStatus();
-
-// UI methods
 void setStatus();
 
 #endif
